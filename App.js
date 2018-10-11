50e619e5897813edc5a481337ea77d4e4de79c84
yogie.putra8 <yogie.putra8@gmail.com>
2018-10-11 20:43:43 +0700

    init commit

---
+++ App.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Login from "./src/Login";
+
+export default class App extends Component {
+  render() {
+    return <Login />;
+  }
+}
